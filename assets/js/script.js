2f8b616a654a9892ec33e1b37ee93e2aa8c156e8
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-09-22 19:08:49 -0500

    form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+
+var formEl = document.querySelector("#task-foprm");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+
+    
+};
+
+formEl.addEventListener("submit", createTaskHandler);
+
+
+
 
 //key in dynamically creating elements witht the DOM
 //create a new task item/ style the new task item/ add the text/ append this element to the task list
-buttonEl.addEventListener("click", createTaskHandler); 
+//buttonEl.addEventListener("click", createTaskHandler);
 
